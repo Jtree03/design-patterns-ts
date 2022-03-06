3e42c6d8940f062a5707266b6da320db5092617e
jtree03 <wowns0903@gmail.com>
2022-03-06 20:22:12 +0900

    Del: proxy pattern's console.log

--- design-patterns/02. proxy/proxy.ts
+++ design-patterns/02. proxy/proxy.ts
@@ -11,13 +11,9 @@ export const personProxy = (person: Person) =>
     get: (obj, prop) => {
       const value = Reflect.get(obj, prop);
 
-      console.log(`value of the ${prop.toString()} is ${value}`);
-
       return value;
     },
     set: (obj, prop, value) => {
-      console.log(`Changed ${prop.toString()} from ${obj[prop]} to ${value}`);
-
       return Reflect.set(obj, prop, value);
     },
   });
